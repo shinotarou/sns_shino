ae95c0471affc57e1e11da197155705136e7e506
LAPTOP-JJFCM8HC\shinohara taisei <s.taisei.goole@gmail.com>
2022-11-14 16:12:12 +0900

    あいまい検索機能を実装しました

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,8 +11,16 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
-    public function search(){
+    public function search(Request $request){
         $users = DB::table('users')->get();
+
+        if (request('search')) {
+            $search = $request->input('search');
+            $users = DB::table('users')
+            ->where('username','like',"%{$search}%")
+            ->get();
+        }
+
         return view('users.search',compact('users'));
     }
 }
